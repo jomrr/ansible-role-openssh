b4ddcafcc94e9b1315f96484ff8bd97f5e348902
Jonas Mauer <jam@kabelmail.net>
2019-06-28 20:43:31 +0200

    added key generation, in pytest it got stuck

--- molecule/tests/test_sshd.py
+++ molecule/tests/test_sshd.py
@@ -7,15 +7,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_sshd_active(host):
+    # this is testing sshd_config
     assert host.service("sshd").is_running is True
-
-
-def test_ssh_create_key(host):
-    key = 'yes | ssh-keygen -q -t rsa -N "" -f /root/.ssh/id_rsa >/dev/null'
-    cmd = host.run(key)
-    assert cmd.rc == 0
-    ak = host.run('cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys')
-    assert ak.rc == 0
 
 
 def test_ssh_login(host):
@@ -24,5 +17,6 @@ def test_ssh_login(host):
     # debian 8 fails, but works without any error when you try manually!?!
     # so this is a todo for times when there is more time ;-)
     if os != 'debian':
+        # this is testing sshd and ssh_config
         login = host.run('ssh -oStrictHostKeyChecking=no localhost')
         assert login.rc == 0
