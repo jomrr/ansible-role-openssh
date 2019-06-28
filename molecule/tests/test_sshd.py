89c4ad0c52b8345b4383ec6da9e2ce43e17ee9b9
Jonas Mauer <jam@kabelmail.net>
2019-06-28 16:32:03 +0200

    test login in verify step

--- molecule/tests/test_sshd.py
+++ molecule/tests/test_sshd.py
@@ -8,3 +8,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_sshd_active(host):
     assert host.service("sshd").is_running is True
+
+def test_ssh_create_key(host):
+    cmd = host.run('yes | ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa >/dev/null')
+    assert cmd.rc == 0
+    cmd = host.run('cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys')
+    assert cmd.rc == 0
+
+def test_ssh_login(host):
+    cmd = host.run('ssh -oStrictHostKeyChecking=no localhost')
+    assert cmd.rc == 0
